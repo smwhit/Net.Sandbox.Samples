a5d68c97f2134626987dee40ceb39269bf85275c
Simon Whittemore <simon.whittemore@gmail.com>
2012-10-28 16:19:57 +0000

    IIS hosting example

---
+++ IISClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IISClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.RestaurantOrderClient client = new ServiceReference1.RestaurantOrderClient();
+            Console.WriteLine(client.MakeOrder(new string[0]));
+            Console.ReadLine();
+        }
+    }
+}
