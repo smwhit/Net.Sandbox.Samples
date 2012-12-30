4796b26f8a9fe533c9a58eee4dc89cc0fcca0bd9
Simon Whittemore <simon.whittemore@gmail.com>
2012-12-30 21:43:03 +0000

    Web API Hello World

---
+++ WebAPI/MvcApplication1/MvcApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] {
+            new Product { Id = 1, Category = "Groceries", Name= "Tomato Soup", Price = 1.0m},
+            new Product { Id = 2, Category = "Toys", Name= "Yo Yo", Price = 1.0m},
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return product;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return products.Where(p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
