3e5f95ececb24574e419b9b282865b53810bb4dd
Simon Whittemore <simon.whittemore@gmail.com>
2013-01-01 12:02:21 +0000

    Web api crud sample

---
+++ WebAPI/MvcApplication1/ProductStore/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll() .Where(p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id});
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        // need to specify id in the query string, and send product as the json payload
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        //public HttpResponseMessage DeleteProduct(int id)
+        //if you have the above signature, you need to specify the id in the query string, model binding does not match the action method
+        public HttpResponseMessage DeleteProduct(Product product)
+        {
+            Product item = repository.GetAll().FirstOrDefault(p => p.Id == product.Id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            repository.Remove(item.Id);
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+
+    }
+}
